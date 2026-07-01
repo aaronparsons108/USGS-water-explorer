0ab2878fa79bf26edb675348fd8bd9e11b91a439
aaronparsons108 <aaronbparsons@gmail.com>
2026-06-30 20:47:08 -0400

    small change

--- pulling-scripts/pullingsettings.py
+++ pulling-scripts/pullingsettings.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import dataretrieval as nwis
+from IPython.display import display #Doing display(obj) rather than print(obj)
+
+all_sites = []
+output_path = (f"{pmcode}_data.csv")
+pmcode = "99133"
+
 states = [
         "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
         "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
